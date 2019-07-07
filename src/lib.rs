39367da8a42b12817c5dccc683e3651a33a792b1
Jonathan Pulfer <pulfer@gmail.com>
2019-07-07 21:03:00 +0100

    First step to creating log event

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,74 @@
 use chrono::Utc;
-use serde::Serializer;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::fmt;
 
 mod iron_logger;
 
-/// Context for the event to record in the log.
-#[derive(Debug)]
-pub struct LogContext {
-    pub fields: HashMap<String, String>,
+/// Classic syslog style levels.
+pub enum Level {
+    DEBUG(Event),
+    INFO(Event),
+    WARN(Event),
+    ERROR(Event),
+    CRITICAL(Event),
+}
+
+impl Level {
+    pub fn debug(event: Event) -> Level {
+        Level::DEBUG(event)
+    }
+
+    pub fn info(event: Event) -> Level {
+        Level::INFO(event)
+    }
+
+    pub fn warn(event: Event) -> Level {
+        Level::WARN(event)
+    }
+
+    pub fn error(event: Event) -> Level {
+        Level::ERROR(event)
+    }
+
+    pub fn critical(event: Event) -> Level {
+        Level::CRITICAL(event)
+    }
+}
+
+/// Simple event to record to the output channel.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Event {
+    payload: HashMap<String, String>,
     created: chrono::DateTime<Utc>,
 }
 
-impl LogContext {
-    pub fn new() -> LogContext {
-        LogContext {
+impl Event {
+    pub fn new() -> Event {
+        Event {
+            payload: HashMap::new(),
             created: chrono::Local::now().with_timezone(&Utc),
-            fields: HashMap::new(),
+        }
+    }
+
+    fn out(&self) -> String {
+        let results = serde_json::to_string(self);
+        match results {
+            Ok(result) => { result }
+            Err(_) => { String::new() }
         }
     }
 }
 
-/// A data object that can be used as a loggable event.
-pub trait Loggable {
-    fn add(&mut self, key: String, value: impl Serializer) -> Self;
+impl fmt::Display for Event {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.out())
+    }
 }
 
-/// Classic syslog style levels.
-pub enum Event {
-    DEBUG(Loggable),
-    INFO(Loggable),
-    WARN(Loggable),
-    ERROR(Loggable),
-    CRITICAL(Loggable),
-}
-
-impl Event {
-    pub fn debug(event: impl Loggable) -> Event {
-        Event::DEBUG(event)
-    }
-
-    pub fn info(event: impl Loggable) -> Event {
-        Event::INFO(event)
-    }
-
-    pub fn warn(event: impl Loggable) -> Event {
-        Event::WARN(event)
-    }
-
-    pub fn error(event: impl Loggable) -> Event {
-        Event::ERROR(event)
-    }
-
-    pub fn critical(event: impl Loggable) -> Event {
-        Event::CRITICAL(event)
-    }
+#[test]
+fn test_event_display() {
+    let mut ev = Event::new();
+    ev.payload.insert("field".to_string(), "value".to_string());
+    println!("{}", ev);
 }
